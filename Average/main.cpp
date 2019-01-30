036cb00003c62980ebcbceb731944178bc301ddc
micapalosa <46881332+micapalosa@users.noreply.github.com>
2019-01-30 16:54:12 +0800

    Add files via upload

---
+++ Average/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i1=85,i2=90,i3=75, sum=0, average=0;
+    sum=i1+i2+i3;
+    average= sum/3;
+    cout << "The Average of the Three given Value is" << average<<endl;
+    return 0;
+}
