346b6ffdbcaa04d05221b936b0265aaa9452a919
micapalosa <46881332+micapalosa@users.noreply.github.com>
2019-01-30 16:55:35 +0800

    Add files via upload

---
+++ Input Radius/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int pi=3.1416, r, area=0;
+    cout << "Input Radius " << endl;
+    cin >> r;
+    area=pi*r*r;
+    cout << "The Area of the circle is=" <<area<< endl;
+    return 0;
+}
