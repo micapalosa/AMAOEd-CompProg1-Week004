36a3c24b01b5b34d8e5349c80c481cb18489e330
micapalosa <46881332+micapalosa@users.noreply.github.com>
2019-01-30 16:51:22 +0800

    Add files via upload

---
+++ Area/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int pi= 3.14159265, r=5, area=0;
+  area=pi*r*r;
+  cout<< "The area of the Circle is" << area << endl;
+    return 0;
+}
