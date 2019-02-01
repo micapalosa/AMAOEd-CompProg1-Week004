63c92c55992b1b1978bfa076509e075b4d02e1cf
micapalosa <46881332+micapalosa@users.noreply.github.com>
2019-02-01 12:25:46 +0800

    Add files via upload

---
+++ VolumeOfSphere/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int pi=3.1416, r=5,sum=0, V=0;
+    sum = 4*pi*r*r;
+    V=sum/3;
+    cout << "The area of the Sphere is" <<V<<endl;
+    return 0;
+}
