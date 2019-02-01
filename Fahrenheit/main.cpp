977f72292f8a3216397e8278b3d7bb4fc3b9fb8f
micapalosa <46881332+micapalosa@users.noreply.github.com>
2019-02-01 12:32:48 +0800

    Add files via upload

---
+++ Fahrenheit/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int f=50,c=0, sum=0;
+    c=(f-32)*5/9;
+    cout << "50 Fahrenheit is equivalent to" << c <<" Celsius "<< endl;
+    return 0;
+}
